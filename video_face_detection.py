20c12026bc2a59a6f80994d9b6026a266bbb6591
artificialnouveau <46301511+artificialnouveau@users.noreply.github.com>
2023-07-22 19:38:54 +0200

    Update video_face_detection.py

--- video_face_detection.py
+++ video_face_detection.py
@@ -1,68 +1,59 @@
-import sys
 import cv2
 import face_recognition
+import datetime
 import os
 import numpy as np
-import datetime
-from PIL import Image, ImageDraw, ImageFont
-
-# Declare output directory as a global variable
-output_dir = ""
-known_face_encodings = []
-
-def save_face(face_image):
-    global output_dir
-    now = datetime.datetime.now()
-    timestamp_str = now.strftime("%Y-%m-%d %H:%M:%S")
-    img_path = os.path.join(output_dir, timestamp_str + '.jpg')
-
-    # Save the image
-    face_image_pil = Image.fromarray(face_image)
-    draw = ImageDraw.Draw(face_image_pil)
-    font = ImageFont.truetype('/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf', 15)
-    draw.rectangle(((0, face_image.shape[0] - 30), (face_image.shape[1], face_image.shape[0])), fill="black")
-    draw.text((10, face_image.shape[0] - 30), timestamp_str, font=font, fill=(255, 255, 255, 128))
-
-    face_image_pil.save(img_path)
-
+from PIL import ImageDraw
+from tqdm import tqdm
 
 def process_frame(frame):
     # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
     rgb_frame = frame[:, :, ::-1]
 
-    # Find all the faces and face enqcodings in the current frame of video
+    # Find all the faces in the current frame of video
     face_locations = face_recognition.face_locations(rgb_frame)
-    face_encodings = face_recognition.face_encodings(rgb_frame, face_locations)
 
-    for (top, right, bottom, left), face_encoding in zip(face_locations, face_encodings):
-        # See if the face is a match for the known face(s)
-        matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
+    return face_locations
 
-        if True not in matches:
-            known_face_encodings.append(face_encoding)
-            save_face(frame[top:bottom, left:right])
+def save_face(image, top, right, bottom, left, output_dir, timestamp):
+    # Crop out the face from the frame
+    face_image = image[top:bottom, left:right]
 
-def main(video_path, output_directory):
-    global output_dir
-    output_dir = output_directory
+    pil_image = Image.fromarray(face_image)
+    draw = ImageDraw.Draw(pil_image)
+    # Use default font
+    draw.text((0, face_image.shape[0] - 30), timestamp, fill="white")
+    filename = f"{output_dir}/{timestamp}.jpg"
+    pil_image.save(filename)
 
+def main(video_path, output_dir):
     video_capture = cv2.VideoCapture(video_path)
 
-    while True:
+    # Get total frames
+    total_frames = int(video_capture.get(cv2.CAP_PROP_FRAME_COUNT))
+
+    for _ in tqdm(range(total_frames), "Processing"):
         # Grab a single frame of video
         ret, frame = video_capture.read()
-        process_frame(frame)
-
-        # Display the resulting image
-        cv2.imshow('Video', frame)
-
-        # Hit 'q' on the keyboard to quit!
-        if cv2.waitKey(1) & 0xFF == ord('q'):
+        if not ret:
             break
 
-    # Release handle to the webcam
+        # Get frame timestamp
+        frame_time = video_capture.get(cv2.CAP_PROP_POS_MSEC)
+        timestamp = datetime.datetime.fromtimestamp(frame_time/1000.0).strftime("%Y_%m_%d-%H_%M_%S-%f")
+
+        face_locations = process_frame(frame)
+        for top, right, bottom, left in face_locations:
+            save_face(frame, top, right, bottom, left, output_dir, timestamp)
+
     video_capture.release()
-    cv2.destroyAllWindows()
 
 if __name__ == "__main__":
-    main(sys.argv[1], sys.argv[2])
+    import argparse
+
+    parser = argparse.ArgumentParser(description='Process and save faces from a video.')
+    parser.add_argument('--video_path', type=str, help='Path to the video file.')
+    parser.add_argument('--output_dir', type=str, help='Directory to save the output images.')
+    args = parser.parse_args()
+
+    main(args.video_path, args.output_dir)
